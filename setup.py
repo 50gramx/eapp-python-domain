1b8c7af5e01937c867ec164c83028f558546f08b
Amit-Khetan-70 <amit.khetan.70@50gramx.io>
2022-10-18 08:15:30 +0530

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='eapp-python-domain',
+    version='0.0.1',
+    author='Amit Khetan',
+    author_email='amit.khetan.70@50gramx.io',
+    description='ethos applications entities and service contracts for python domain',
+    packages=['eapp-python-domain'],
+    install_requires=['protobuf', 'grpcio', 'grpcio-tools'],
+)
