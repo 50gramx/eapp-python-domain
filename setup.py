4526ba6e83d002f830e28de5f2fc5d4f335df2c1
Amit-Khetan-70 <amit.khetan.70@50gramx.io>
2022-11-10 05:10:53 +0530

    Released v0.2.10

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='eapp-python-domain',
-    version='0.2.9',
+    version='0.2.10',
     author='Amit Khetan',
     author_email='amit.khetan.70@50gramx.io',
     description='ethos applications entities and service contracts for python domain',
