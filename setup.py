95f6c461f421b7f254b1d6764ec8c512a7daa99e
Amit-Khetan-70 <amit.khetan.70@50gramx.io>
2022-11-10 02:21:20 +0530

    ELINT-72 Updated init, updated find_packages

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setuptools.setup(
     author='Amit Khetan',
     author_email='amit.khetan.70@50gramx.io',
     description='ethos applications entities and service contracts for python domain',
-    packages=['eapp-python-domain'],
+    packages=setuptools.find_packages('eapp-python-domain.eapp-python-domain'),
     install_requires=['protobuf', 'grpcio', 'grpcio-tools'],
+    include_package_data=True
 )
