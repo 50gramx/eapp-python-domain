fd6c5ebf568a5518e989939f00d61c21f114ad65
Amit Khetan <amit.khetan.70@50gramx.io>
2023-09-14 23:16:13 +0000

    fix packages

--- setup.py
+++ setup.py
@@ -7,15 +7,12 @@ here = pathlib.Path(__file__).parent.resolve()
 # NOTE: DO NOT EDIT ANYTHING TILL LINE NO 10
 setup(
     name='eapp_python_domain',
-    version='0.2.21',
+    version='0.2.22',
     author='Amit Khetan',
     author_email='amit.khetan.70@50gramx.io',
     description='ethos applications entities and service contracts for python domain',
     package_dir = {"": "src"},
-    packages=find_packages(
-        where='src',
-        include=['eapp_python_domain*'],
-    ),
+    packages=['eapp_python_domain'],
     install_requires=['protobuf', 'grpcio', 'grpcio-tools'],
     include_package_data=True
 )
