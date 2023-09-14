f73e84ab999004403e542c7ba07eba16722d5d13
Amit Khetan <amit.khetan.70@50gramx.io>
2023-09-14 22:54:28 +0000

    add package_dir

--- setup.py
+++ setup.py
@@ -7,10 +7,11 @@ here = pathlib.Path(__file__).parent.resolve()
 # NOTE: DO NOT EDIT ANYTHING TILL LINE NO 10
 setup(
     name='eapp_python_domain',
-    version='0.2.20',
+    version='0.2.21',
     author='Amit Khetan',
     author_email='amit.khetan.70@50gramx.io',
     description='ethos applications entities and service contracts for python domain',
+    package_dir = {"": "src"}
     packages=find_packages(),
     install_requires=['protobuf', 'grpcio', 'grpcio-tools'],
     include_package_data=True
